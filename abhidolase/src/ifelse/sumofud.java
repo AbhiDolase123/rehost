fdf6995a86f92fac5ae6a827d1d41b9ce2ed077c
Lenovo <Lenovo@192.168.2.8>
2021-09-12 01:48:05 -0700

    sum of first and last digit

---
+++ abhidolase/src/ifelse/sumofud.java
@@ -0,0 +1,39 @@
+package ifelse;
+import java.util.*;
+public class sumofud {
+	
+	
+	   public static void main(String args[])
+	   {
+
+		int num;
+		Scanner sc =new Scanner(System.in);
+		
+		System.out.println("Enter The Number:");
+		
+		num=sc.nextInt();
+		
+		if(num>99 && num<1000)
+		{
+			System.out.println("Three Digit Number.");
+			
+			int lastdigit = num%10;
+			
+			int firstdigit=num;
+			
+			while(firstdigit>=10)
+			{
+				firstdigit=firstdigit/10;
+			}
+			System.out.println("firstdigit="+firstdigit);
+			System.out.println("lastdigit="+lastdigit);
+			System.out.println("Sum="+(firstdigit+lastdigit));
+		}
+		else
+			System.out.println("Not a 3 Digit Number");
+		
+		
+
+	   }
+	}
+
