4d0a693da71d04173280d2210e2a6f7b6963893d
Lenovo <Lenovo@192.168.2.8>
2021-09-12 03:10:34 -0700

    dd

---
+++ abhidolase/src/array/onedarray.java
@@ -0,0 +1,25 @@
+package array;
+
+public class onedarray {
+	
+			public static void main(String[] args)
+			{
+				int array[]=new  int [30];
+	              array[0]=20;
+	             array[1]=30;
+	             array[3]=20;
+
+
+				System.out.println("frist"+array[0]);
+				System.out.println("second"+array[2]);
+				System.out.println("3rd"+array[2]);
+
+			}
+			
+			
+
+
+	}
+
+
+
