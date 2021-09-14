069a2523cd48751b2023b13f89dbfb8d4fed7ddd
Lenovo <Lenovo@192.168.2.8>
2021-09-14 08:23:21 -0700

    o

---
+++ abhidolase/src/uniquepair/outerel.java
@@ -0,0 +1,30 @@
+package uniquepair;
+import java.util.*;
+
+public class outerel {
+
+		public static void main(String[] args) {
+			int[][] arr = new int[4][4];
+			Scanner s = new Scanner(System.in);
+			System.out.print("Enter elemts");
+
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					arr[i][j] = s.nextInt();
+				}
+			}
+
+			
+			  for(int i=0;i<4;i++) 
+			  { 
+				  for(int j=0;j<4;j++) 
+				  {
+					  if(i == 0 || j==0 || i==3 ||j==3)
+						  System.out.print(arr[i][j]);
+				  } 
+			  } 
+			 
+		}
+	}
+
+
