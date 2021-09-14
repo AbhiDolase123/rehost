1e60a8fe4a70fd893abcfa81568c0cb08e06bf35
Lenovo <Lenovo@192.168.2.8>
2021-09-14 07:28:15 -0700

    jj

---
+++ abhidolase/src/loop/productofunit.java
@@ -0,0 +1,25 @@
+package loop;
+import java.util.Scanner;
+
+public class productofunit {
+
+	
+
+		public static void main(String[] args) {
+			{
+		        int n,mul=1,k;
+		        System.out.println("Enter the Numbers :");
+		        Scanner sc= new Scanner(System.in);
+		        n=sc.nextInt();
+		        for(mul=1; n >0;n = n / 10)
+		        {
+		            k = n%10;
+		            mul = mul*k;
+		        }
+		        System.out.println("Product of Digits of Number : "+mul);
+		    }
+
+		}
+
+	
+}
